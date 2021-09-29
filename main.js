2d868b563a716ed82fc0b91d09ce6c9e528249e8
rk2810@mail.ru <rk2710@mail.ru>
2021-09-29 09:00:47 +0300

    hhh

---
+++ main.js
@@ -0,0 +1,44 @@
+import "./css/style.scss";
+
+
+let data = [
+    { id: 1, title: 'Мельница у реки', path: "img/1.jpg", type: "img" },
+    { id: 2, title: 'Горы осенью', path: "img/2.jpg", type: "img" },
+    { id: 3, title: 'Деревня летом', path: "img/3.jpg", type: "img" },
+    { id: 4, title: 'Анимированный котик', path: "img/4.gif", type: "img" },
+    { id: 5, title: 'Пение птиц', path: "audio/1.mp3", type: "audio" },
+    { id: 6, title: 'Пение птиц', path: "audio/2.mp3", type: "audio" },
+    { id: 7, title: 'Пение птиц', path: "audio/3.mp3", type: "audio" },
+    { id: 8, title: 'Пение птиц', path: "video/1.mp4", type: "video" },
+    { id: 9, title: 'Пение птиц', path: "video/2.mp4", type: "video" },
+];
+const renderProduct = (card) => {
+    if (card.type === "img") {
+        return `<div class="photos">
+    <img class="pictures" src="${card.path}"> 
+    <h3 class="names">${card.title}</h3>
+    </div>`}
+
+    else if (card.type === "audio") {
+        return `<div class="photos">
+       
+    <audio class="pictures" controls poster="img/1.jpg">
+    <source src="${card.path}" type="audio/mpeg">
+  </audio>
+  <h3 class="names">${card.title}</h3>
+    </div>`}
+
+    else
+        return `<div class="photos">
+    <video class="pictures" controls>
+        <source src="${card.path}">
+    </video>
+    <h3 class="names">${card.title}</h3>
+    </div>`
+
+};
+
+const renderAllProdact = data.map(item => renderProduct(item)).join("");
+document.querySelector(".content").insertAdjacentHTML("afterbegin", renderAllProdact);
+
+
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+
+
+
+module.exports = {
+    mode: 'development',
+    entry: './main.js',
+    output: {
+        filename: 'main.[contenthash].js'
+    },
+
+
+    devServer: {
+        port: 9000,
+        hot: true,
+        static: './'
+    },
+
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+           
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                           
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        },
+                    },
+                ],
+            }
+
+        ],
+    },
+
+    plugins: [
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({ template: resolve(__dirname, 'index.html') }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: "img", to: "img" },
+                { from: "video", to: "video" },
+                { from: "audio", to: "audio" }
+            ],
+        }),
+
+        new BundleAnalyzerPlugin(),
+    ],
+
+}
